7f4bcabfb6489d0f9784f0097734cc7dc3b1342c
Steven Morgan <sum410@psu.edu>
2018-09-20 11:15:48 -0400

    Extend regex's

--- Headnotes/find_all_citations.py
+++ Headnotes/find_all_citations.py
@@ -44,40 +44,66 @@ for entry in cleandirlist: ## each entry is a txt file with an opinion
     infilehandle = open(infilepath)
     txtlines = infilehandle.readlines()
 
-    for txtline in txtlines:
-        if re.search('HN2', txtline):
-        #if re.search(' v. ', txtline):
-            #print txtline
 
-            if re.search("([\s]Fed\.\sAppx\.|[\s]F\.3d|[\s]F\.[\s]|[\s]F\.2d|[\d] U.S. [\d]|[\s]App\.)", txtline):
-                print "FOUND IT"
+    for i in range(1,11):
 
-            m = re.search('\\b(\\d{1,3})\\s([AFSNU]\\.\\s?[\\w\\.]+)\\s(\\d{1,4}|_{1,4})\\b', txtline)
-            if m:
-                reporter_hn = m.group()
-                print reporter_hn
+        hnCounter = 'HN' + str(i)
+        print '\n'
+        print repr(hnCounter)
 
-            m2 = re.search('\\b(\\d{1,3})\\s[S]\\.\\s[C][t]\\.\\s(\\d{1,4}|_{1,4})\\b', txtline)
-            if m2:
-                sct_cite_hn = m2.group()
+        for txtline in txtlines:
 
-                print repr(sct_cite_hn)
+            if re.search(hnCounter, txtline):
 
-            m3 = re.search('\\b(\\d{1,3})\\s([A-Z]\\.)\\s([A-Z][a-z]\\.)\\s(\\d[a-z])\\s(\\d{1,3}|_{1,3})\\b', txtline)
-            if m3:
-                lexis_cite_hn = m3.group()
-                print repr(lexis_cite_hn)
-                
-            #subText = "([\s]Fed\.\sAppx\.|[\s]F\.3d|[\s]F\.[\s]|[\s]F\.2d|[\d] U.S. [\d]|[\s]App\.)"
-            #start = FindSubString(txtline, subText, 0)
-            #print start
+                '''
+                m = re.search('(?:review: )?(\\b(\\d{1,3})\\s([AFSNU]\\.\\s?[\\w\\.]+)\\s(\\d{1,4}|_{1,4})\\b)', txtline)
+                if m:
+                    reporter_hn = m.group()
+                    #print reporter_hn
 
-            #print txtline.split(subText)[0]
+                m2 = re.search('\\b(\\d{1,3})\\s[S]\\.\\s[C][t]\\.\\s(\\d{1,4}|_{1,4})\\b', txtline)
+                if m2:
+                    sct_cite_hn = m2.group()
 
-            #found = re.search('([\s]Fed\.\sAppx\.|[\s]F\.3d|[\s]F\.[\s]|[\s]F\.2d|[\d] U.S. [\d]|[\s]App\.)', txtline)#.group(1)
-            #print found
+                    #print repr(sct_cite_hn)
 
-            # For the first instance of a 'HNX', need to go back one line to
-            # grab the text of the headnote (i.e. Constitutional Law > ... >
-            # Case or Controversy > Standing > General Overview Civil Procedure > ... > Justiciability > Standing > General Overview)
-            #sys.exit()
+                m3 = re.search('\\b(\\d{1,3})\\s([A-Z]\\.)\\s([A-Z][a-z]\\.)\\s(\\d[a-z])\\s(\\d{1,3}|_{1,3})\\b', txtline)
+                if m3:
+                    lexis_cite_hn = m3.group()
+                    #print repr(lexis_cite_hn)
+                '''
+
+                txtline = txtline.split(hnCounter)[1]
+
+
+                report_pattern = re.compile('(\\b\\d{1,3}\\s[AFSNU]\\.\\s?[\\w\\.]+\\s\\d{1,4}|_{1,4}\\b)')
+                report_match = report_pattern.findall(txtline)
+                #print report_match
+
+                sct_pattern = re.compile('(\\b\\d{1,3}\\s[S]\\.\\s[C][t]\\.\\s\\d{1,4}|_{1,4}\\b)')
+                sct_match = sct_pattern.findall(txtline)
+                #print sct_match
+
+                lexis_pattern = re.compile('(\\b\\d{1,3}\\s[A-Z]\\.\\s[A-Z][a-z]\\.\\s\\d[a-z]\\s\\d{1,3}|_{1,3}\\b)')
+                lexis_match = lexis_pattern.findall(txtline)
+                #print lexis_match
+
+                # For the first instance of a 'HNX', need to go back one line to
+                # grab the text of the headnote (i.e. Constitutional Law > ... >
+                # Case or Controversy > Standing > General Overview Civil Procedure > ... > Justiciability > Standing > General Overview)
+                #sys.exit()
+
+        try:
+            print report_match
+            print repr('\n')
+        except:
+            pass
+        try:
+            print sct_match
+            print repr('\n')
+        except:
+            pass
+        try:
+            print lexis_match
+        except:
+            pass
