b025600e831714d6ff06cbce487c2af67b01f41e
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-10-02 20:31:51 -0400

    NER Update

---
+++ ner_test.py
@@ -0,0 +1,66 @@
+# This code test NER on person recognition of judge lines in case files
+
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
+
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    prev = None
+    continuous_chunk = []
+    current_chunk = []
+    for i in chunked:
+            if type(i) == Tree:
+                    current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+            elif current_chunk:
+                    named_entity = " ".join(current_chunk)
+                    if named_entity not in continuous_chunk:
+                            continuous_chunk.append(named_entity)
+                            current_chunk = []
+            else:
+                    continue
+    return continuous_chunk
+
+my_sent = "WASHINGTON -- In the wake of a string of abuses by New York police officers in the 1990s, Loretta E. Lynch, the top federal prosecutor in Brooklyn, spoke forcefully about the pain of a broken trust that African-Americans felt and said the responsibility for repairing generations of miscommunication and mistrust fell to law enforcement."
+print get_continuous_chunks(my_sent)
+
+'''
+import nltk
+from nameparser.parser import HumanName
+
+def get_human_names(text):
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentt = nltk.ne_chunk(pos, binary = False)
+    person_list = []
+    person = []
+    name = ""
+    for subtree in sentt.subtrees(filter=lambda t: t.node == 'PERSON'):
+        for leaf in subtree.leaves():
+            person.append(leaf[0])
+        if len(person) > 1: #avoid grabbing lone surnames
+            for part in person:
+                name += part + ' '
+            if name[:-1] not in person_list:
+                person_list.append(name[:-1])
+            name = ''
+        person = []
+
+    return (person_list)
+
+sentence = 'Judge Kavanaugh went to school.'
+get_human_names(sentence)
+'''
+
+# Stanford NER solution
+'''
+import nltk
+from nltk.tag.stanford import NERTagger
+st = NERTagger('stanford-ner/all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
+text = """YOUR TEXT GOES HERE"""
+
+for sent in nltk.sent_tokenize(text):
+    tokens = nltk.tokenize.word_tokenize(sent)
+    tags = st.tag(tokens)
+    for tag in tags:
+        if tag[1]=='PERSON': print tag
+'''
